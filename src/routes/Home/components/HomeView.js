34cde3ec695563d76bb67e4bd63837d302ab7be5
Daniel <danbernardi1@gmail.com>
2017-06-15 14:40:50 -0700

    Tests breakpoint function

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import DuckImage from 'routes/Home/assets/Duck.jpg'
+import { connect } from 'react-redux';
+import { setClass } from 'utils/responsiveHelpers';
 import './HomeView.scss'
 
-export const HomeView = () => (
-  <div className="py4">
+export const HomeView = ({ breakpoint }) => (
+  <div className={ setClass({ default: 'py6', mobileLg: 'py3' }, breakpoint) }>
     <h4 className="typ--center">Welcome!</h4>
     <img alt='This is a duck, because Redux!' className='duck' src={DuckImage} />
   </div>
 )
 
-export default HomeView
+const mapStateToProps = state => ({
+  breakpoint: state.breakpoint
+});
+
+export default connect(mapStateToProps)(HomeView)
