25791506565e6d5bdaad39becfacfc46e0622c82
Daniel <danbernardi1@gmail.com>
2017-06-15 11:36:33 -0700

    Adds breakpoint helper functions / handlers / actions

---
+++ src/store/handlers.js
@@ -0,0 +1,16 @@
+/**
+ * Object defining current breakpoint state
+ * @type {Object}
+ */
+export const breakpoint = {
+  _init: { name: 'default', size: null, queryState: [] },
+  SET_ACTIVE_BREAKPOINT: (state, action) => {
+    const newobj = {
+      name: action.breakpointName,
+      size: action.breakpointSize,
+      queryState: action.queryState
+    };
+
+    return newobj;
+  }
+};
